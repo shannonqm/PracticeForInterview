7aa67fb7930b321626a2c8410592d7eeb0387653
DESKTOP-5ONF5IG\Administrator <shangqingmao@126.com>
2019-05-18 15:36:10 +0800

    GCtest

---
+++ PracticeForInterview/Assets/GCTest/ComponentTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentTest : MonoBehaviour {
+
+    public GameObject obj;
+    //public Collider myCollider;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(myCollider.name);
+        Debug.Log(obj.GetComponent<Collider>().name);
+	}
+}
